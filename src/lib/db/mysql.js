056d273ef19df5aa2dd3c0336a2dc21733fd78cd
Eni Hasmegaj <EniHas20@htl-shkoder.com>
2025-01-28 12:29:37 +0100

    ADD ENV FILE ODER WAS IHR VOLLT

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,14 +1,20 @@
 import mysql from 'mysql2/promise';
+import {
+	DB_NAME,
+	DB_PASSWORD,
+	DB_PORT,
+	DB_HOST,
+	DB_USER } from '$env/static/private';
 
 let connection = null;
 export function createConnection() {
 	if (!connection) {
 		connection = mysql.createConnection({
-			host: 'htl-projekt.com',
-			user: 'enihasmegaj',
-			port: 33060,
-			password: '!Insy_2024$',
-			database: '2024_4bw_enihasmegaj_event_app'
+			host: DB_HOST,
+			user: DB_USER,
+			port: DB_PORT,
+			password: DB_PASSWORD,
+			database: DB_NAME
 		});
 	}
 	return connection;
